3ebff896a8eefacbcab42eff667f5f57334d11d9
cosmic-alth <ameerafran90@gmail.com>
2026-02-28 20:59:59 +0530

    Update env.js

--- env.js
+++ env.js
@@ -1,6 +1,5 @@
-// Global Configuration
 const CONFIG = {
-    GEMINI_KEY: 'AIzaSyDy0VUOoQzEhfa4JOX_X1Zxj8KDHXDCtHM', // Your Gemini Key
-    NEWS_KEY: 'pub_2c5d5364391f4045945da79ca4f87a50',     // Your NewsData Key
-    FLIGHT_KEY: 'd5e173661faa7a4b822797e42666b6f3'       // Your Flight Key
+    GEMINI_KEY: 'AIzaSyBjwmeGW5qWUwls_UKaPCGYynD8q_LUVOI', 
+    NEWS_KEY: 'pub_2c5d5364391f4045945da79ca4f87a50',
+    FLIGHT_KEY: 'd5e173661faa7a4b822797e42666b6f3'
 };
